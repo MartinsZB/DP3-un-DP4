a15e8578acf2f05f5b5db509f5a3872078480df2
Minion Dave <minion@localhost.localdomain>
2018-09-15 22:52:39 +0300

    Timeru dizains

--- app/src/main/java/com/martinszb/dp3undp4/MainActivity.java
+++ app/src/main/java/com/martinszb/dp3undp4/MainActivity.java
@@ -26,4 +26,12 @@ public class MainActivity extends AppCompatActivity {
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
     }
+    public void openTimersDP3(View view){
+        Intent intent = new Intent(this, DP3TimerActivity.class);
+        startActivity(intent);
+    }
+    public void openTimersDP4(View view){
+        Intent intent = new Intent(this, DP4TimerActivity.class);
+        startActivity(intent);
+    }
 }
