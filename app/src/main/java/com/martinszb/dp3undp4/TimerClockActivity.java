d192f81c0df67c000badf099f5b316ddc4df1483
Minion Dave <minion@localhost.localdomain>
2018-09-16 21:32:25 +0300

    Timeri ar skaņu

--- app/src/main/java/com/martinszb/dp3undp4/TimerClockActivity.java
+++ app/src/main/java/com/martinszb/dp3undp4/TimerClockActivity.java
@@ -2,7 +2,9 @@ package com.martinszb.dp3undp4;
 
 import android.content.Intent;
 import android.graphics.Color;
+import android.media.MediaPlayer;
 import android.os.CountDownTimer;
+import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatCallback;
@@ -12,19 +14,36 @@ import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
+import java.util.Locale;
 import java.util.concurrent.TimeUnit;
 
 
 
 public class TimerClockActivity extends AppCompatActivity {
+
+    Boolean clockComp;
+    CountDownTimer myTimer;
+    Boolean isRunning = false;
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
         if (id == android.R.id.home) {
             onBackPressed();
+            if (isRunning){
+                myTimer.cancel();
+            }
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (isRunning){
+            myTimer.cancel();
+        }
+        finish();
     }
 
     @Override
@@ -36,7 +55,7 @@ public class TimerClockActivity extends AppCompatActivity {
         Bundle extras = intent.getExtras();
         String clockType = extras.getString("EXTRA_TYPE");
         Integer clockSession = extras.getInt("EXTRA_SESSION");
-        Boolean clockComp = extras.getBoolean("EXTRA_COMPETITION");
+        clockComp = extras.getBoolean("EXTRA_COMPETITION");
 
         TextView typeText = findViewById(R.id.textView8);
         TextView compText = findViewById(R.id.textView10);
@@ -86,21 +105,39 @@ public class TimerClockActivity extends AppCompatActivity {
 
     private void initTimer() {
         final TextView sessionText = findViewById(R.id.textView9);
-        sessionText.setText(getString(R.string.sagatavoties));
+        if (clockComp){
+            sessionText.setText(getString(R.string.sagatavoties));
+        }
+        else {
+            sessionText.setText(getString(R.string.sagatavoties));
+            MediaPlayer mp = MediaPlayer.create(getApplicationContext(), R.raw.sagatavoties);
+            mp.start();
+        }
     }
 
 
     private void startTimer(long time, final long tick, final long shootTime, final int id) {
         final TextView sessionText = findViewById(R.id.textView9);
         final TextView counterText = findViewById(R.id.textView11);
-        CountDownTimer myTimer = new CountDownTimer(time, tick) {
+        myTimer = new CountDownTimer(time, tick) {
             @Override
             public void onTick(long millisUntilFinished) {
                 if (millisUntilFinished / 1000 == 7 && id == 0) {
-                    sessionText.setText(getString(R.string.uzmanibu));
-                    counterText.setText(String.format("%02d:%02d", (millisUntilFinished / 60000),(millisUntilFinished % 60000 / 1000)));
+                    if (clockComp){
+                        isRunning = true;
+                        sessionText.setText(getString(R.string.uzmanibu));
+                        counterText.setText(String.format("%02d:%02d", (millisUntilFinished / 60000), (millisUntilFinished % 60000 / 1000)));
+                    }
+                    else {
+                        isRunning = true;
+                        sessionText.setText(getString(R.string.uzmanibu));
+                        MediaPlayer mp = MediaPlayer.create(getApplicationContext(), R.raw.uzmanibu);
+                        mp.start();
+                        counterText.setText(String.format("%02d:%02d", (millisUntilFinished / 60000), (millisUntilFinished % 60000 / 1000)));
+                    }
                 }
                 else {
+                    isRunning = true;
                     counterText.setText(String.format("%02d:%02d", (millisUntilFinished / 60000),(millisUntilFinished % 60000 / 1000)));
                 }
             }
@@ -108,11 +145,26 @@ public class TimerClockActivity extends AppCompatActivity {
             @Override
             public void onFinish() {
                 if (id == 0){
-                    sessionText.setText(getString(R.string.starts));
-                    startTimer(shootTime,tick,0,1);
+                    if (clockComp){
+                        sessionText.setText(getString(R.string.starts));
+                        startTimer(shootTime, tick, 0, 1);
+                    }
+                    else {
+                        sessionText.setText(getString(R.string.starts));
+                        MediaPlayer mp = MediaPlayer.create(getApplicationContext(), R.raw.starts);
+                        mp.start();
+                        startTimer(shootTime, tick, 0, 1);
+                    }
                 }
                 else {
-                    sessionText.setText(getString(R.string.stop));
+                    if (clockComp){
+                        sessionText.setText(getString(R.string.stop));
+                    }
+                    else {
+                        sessionText.setText(getString(R.string.stop));
+                        MediaPlayer mp = MediaPlayer.create(getApplicationContext(), R.raw.stop);
+                        mp.start();
+                    }
                 }
 
             }
